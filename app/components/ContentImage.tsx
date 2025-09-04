549cd93ccd910fe452993e5c03d9004d61d8c317
Pramod <pramodrawat4me@gmail.com>
2025-09-04 18:45:31 +0530

    made changes

--- app/components/ContentImage.tsx
+++ app/components/ContentImage.tsx
@@ -1,4 +1,14 @@
+"use client";
+
 import React from "react";
+
+// Button type for ci_button
+interface Button {
+  url: string;
+  title: string;
+}
+
+// Props for ContentImage component
 interface ContentImageProps {
   ci_onoff?: boolean;
   ci_top_title?: string;
@@ -6,10 +16,11 @@ interface ContentImageProps {
   ci_top_description?: string;
   ci_title?: string;
   ci_description?: string;
-  ci_button?: string;
+  ci_button?: Button; // now properly typed
 }
+
 export default function ContentImage({
-  ci_onoff,
+  ci_onoff = false,
   ci_top_title,
   ci_image,
   ci_top_description,
@@ -24,25 +35,21 @@ export default function ContentImage({
           <div className="container">
             <div className="title-topbar text-center">
               {ci_top_title && (
-                <h2 dangerouslySetInnerHTML={{ __html: ci_top_title || "" }} />
+                <h2 dangerouslySetInnerHTML={{ __html: ci_top_title }} />
               )}
               {ci_top_description && (
-                <p
-                  dangerouslySetInnerHTML={{ __html: ci_top_description || "" }}
-                />
+                <p dangerouslySetInnerHTML={{ __html: ci_top_description }} />
               )}
             </div>
+
             <div className="d-flex jc-space-between relaxation-info f-center">
               <div className="relaxation-left">
                 <div className="relaxation-cont">
                   {ci_title && (
-                    <h2 dangerouslySetInnerHTML={{ __html: ci_title || "" }} />
+                    <h2 dangerouslySetInnerHTML={{ __html: ci_title }} />
                   )}
-
                   {ci_description && (
-                    <p
-                      dangerouslySetInnerHTML={{ __html: ci_description || "" }}
-                    />
+                    <p dangerouslySetInnerHTML={{ __html: ci_description }} />
                   )}
                   {ci_button && (
                     <a href={ci_button.url} className="cmn-btn">
@@ -51,18 +58,20 @@ export default function ContentImage({
                   )}
                 </div>
               </div>
+
               <div className="relaxation-right">
-                <div className="relaxation-img">
-                  <img
-                    src={ci_image}
-                    width="701"
-                    height="644"
-                    alt="Our spa is a sanctuary where relaxation meets rejuvenation."
-                  />
-                </div>
+                {ci_image && (
+                  <div className="relaxation-img">
+                    <img
+                      src={ci_image}
+                      width={701}
+                      height={644}
+                      alt="Our spa is a sanctuary where relaxation meets rejuvenation."
+                    />
+                  </div>
+                )}
               </div>
             </div>
-            <div></div>
           </div>
         </section>
       )}
